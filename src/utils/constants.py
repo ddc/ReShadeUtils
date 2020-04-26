8b5e16557896d532f7c6a066e85283af731fb253
ddc <ddc@users.noreply.github.com>
2020-04-26 03:24:55 -0300

    update

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -14,7 +14,7 @@ import sys
 
 from src.utils import utilities
 
-VERSION = "3.6"
+VERSION = "3.7"
 PROGRAM_NAME = "Reshade Utils"
 SHORT_PROGRAM_NAME = "ReshadeUtils"
 FULL_PROGRAM_NAME = f"{PROGRAM_NAME} v{VERSION}"
